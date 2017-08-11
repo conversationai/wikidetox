acb09bf1ac915bd9a318db62b00e814e439eb3e0
vegetable68 <h931125@gmail.com>
2017-08-11 14:56:48 -0400

    bug fixed

--- get_revisions/wikipedia_revisions_ingester.py
+++ get_revisions/wikipedia_revisions_ingester.py
@@ -23,13 +23,15 @@ TALK_PAGE_NAMESPACE = [1, 3, 5, 7, 9, 11, 13, 15]
 class ParserContentHandler(xml.sax.ContentHandler):
   """Content handler using a minimal incremental combinator parser."""
 
-  def __init__(self, data_reset_path, data_paths):
+  def __init__(self, data_reset_path, data_paths, revision_reset_path):
     xml.sax.ContentHandler.__init__(self)
     # The path through the XML to get to where we currently are.
     self.xml_path = xml_path.XmlPath()
     self.data = {}
     self.data_reset_path = data_reset_path
     self.data_paths = data_paths
+    self.revision_reset_path = revision_reset_path
+    self.page_data = {}
 
   def startElement(self, name, attrs):
     self.xml_path.enter(name)
@@ -37,26 +39,38 @@ class ParserContentHandler(xml.sax.ContentHandler):
 
   def endElement(self, name):
     # print('# END: ' + str(self.xml_path))
+    if self.xml_path.element_path_eq(self.revision_reset_path):
+       if int(self.page_data['page_namespace']) in TALK_PAGE_NAMESPACE:
+          if 'revisions' not in self.page_data: self.page_data['revisions'] = [] 
+          self.page_data['revisions'].append(self.data)
+          self.data = {} 
+       
     if self.xml_path.element_path_eq(self.data_reset_path):
-      if int(self.data['page_namespace'][0]) in TALK_PAGE_NAMESPACE: 
-         print(json.dumps(self.data))
-      self.data = {}
+      if int(self.page_data['page_namespace']) in TALK_PAGE_NAMESPACE: 
+         print(json.dumps(self.page_data))
+      self.page_data = {}
     self.xml_path.exit()
 
   def characters(self, content_text):
     self.xml_path.add_line_of_content()
-    if not('page_namespace' in self.data) or \
-       int(self.data['page_namespace'][0]) in TALK_PAGE_NAMESPACE:
+    if not('page_namespace' in self.page_data) or \
+       int(self.page_data['page_namespace']) in TALK_PAGE_NAMESPACE:
        for data_name, data_path in self.data_paths:
          if self.xml_path.element_path_eq(data_path):
-           if data_name not in self.data:
-             self.data[data_name] = []
-           self.data[data_name].append(content_text)
+           if 'page' in data_name:
+              if data_name not in self.page_data:
+                 self.page_data[data_name] = ""
+              self.page_data[data_name] += content_text
+           else:
+              if data_name not in self.data:
+                 self.data[data_name] = ""
+              self.data[data_name] += content_text
     # print('# CONTENT: ' + str(self.xml_path))
 
 
 def main():
   data_reset_path = xml_path.XmlPath().enter_many(['mediawiki', 'page'])
+  revision_reset_path = xml_path.XmlPath().enter_many(['mediawiki', 'page', 'revision']) 
   data_paths = [
       ('comment', xml_path.XmlPath().enter_many(
           ['mediawiki', 'page', 'revision', 'comment'])),
@@ -83,7 +97,7 @@ def main():
           ['mediawiki', 'page', 'revision', 'contributor', 'username'])),
   ]
   content_handler = ParserContentHandler(
-      data_reset_path=data_reset_path, data_paths=data_paths)
+      data_reset_path=data_reset_path, data_paths=data_paths, revision_reset_path=revision_reset_path)
 
   cmd = (['7z', 'x', args.input, '-so']
          if args.input.endswith('.7z') else ['cat', args.input])
