1664418aa20cb52e38013ba79f767bc91f6f7878
Nithum Thain <nthain@nthain-macbookpro1.roam.corp.google.com>
2017-08-26 11:48:19 -0230

    Fixed ImportError: No module named core

--- ingest_revisions_dataflow/setup.py
+++ ingest_revisions_dataflow/setup.py
@@ -100,8 +100,9 @@ class CustomCommands(setuptools.Command):
 # so this dependency will not trigger anything to be installed unless a version
 # restriction is specified.
 REQUIRED_PACKAGES = [
-    'google-cloud >= 0.27.0', 
-    'google-cloud-storage >= 1.3.2',
+    'google-cloud == 0.27.0', 
+    'google-cloud-storage == 1.3.2',
+    'google-apitools == 0.5.10'
     ]
 
 
