6f4173eb39af8a637bc2ba62514bcaef1b6d80f3
vegetable68 <h931125@gmail.com>
2017-09-07 10:46:47 -0400

    changed big query code

--- conversation_reconstruction/conv_pipe.py
+++ conversation_reconstruction/conv_pipe.py
@@ -10,7 +10,7 @@ from pathlib import Path
 def reconstruct(number):
     actions_lst = []
     actions = {}
-    with open('/scratch/wiki_dumps/comments2bq/conversations%d.json'%number, 'r') as f:
+    with open('/scratch/wiki_dumps/updated/conversations%d.json'%number, 'r') as f:
          for line in f:
              action = json.loads(line)
              actions_lst.append(action)
@@ -20,8 +20,17 @@ def reconstruct(number):
     conv_mapping = {}
     actions_lst = sorted(actions_lst, key = lambda k : (int(k['rev_id']), int(k['indentation'])))
 
+    inspect = '67065595.3923.3923'
     for action in actions_lst:
+  
+        if action['id'] == inspect or action['replyTo_id'] == inspect:
+           print(action['id'], action['replyTo_id'], action['conversation_id'])
+           print(action)
+        continue
+"""
         try:
+           if action['type'] == 'COMMENT_REARRANGEMENT':
+              continue
            if action['type'] == 'COMMENT_ADDING' or action['type'] == 'COMMENT_MODIFICATION' \
               or action['type'] == 'SECTION_CREATION':
               if action['replyTo_id'] == None:
@@ -32,14 +41,12 @@ def reconstruct(number):
                  else:
                     action['conversation_id'] = conv_mapping[action['replyTo_id']]
            else:
-              if action['id'] == action['parent_id'] or action['parent_id'] == None:
+              if action['id'] == action['parent_id']:
                  action['conversation_id'] = action['id']
-                 print(action)
               else:
                  action['conversation_id'] = conv_mapping[action['parent_id']]
-           
         except:
-           print(action, actions[action['parent_id']])
+           print(action)
 
         conv_mapping[action['id']] = action['conversation_id']
         with open('/scratch/wiki_dumps/updated/conversations%d.json'%(number), 'a') as w:
@@ -49,6 +56,7 @@ def reconstruct(number):
            assert(action['conversation_id'] == conv_mapping[action['replyTo_id']])
         if action['id'] == '145600288.20051.20063' or action['replyTo_id'] == '145600288.20051.20063':
            print(action['id'], action['replyTo_id'], action['conversation_id'])
+"""
 
 p = Pool(70)
 result = p.map(reconstruct, range(70))
