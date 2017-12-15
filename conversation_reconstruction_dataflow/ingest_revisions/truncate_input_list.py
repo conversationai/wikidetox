dab61aef5e5d53fd483869c1029954c78ab3841e
Yiqing <h931125@gmail.com>
2017-12-15 20:32:58 +0100

    change according to comments

--- conversation_reconstruction_dataflow/ingest_revisions/truncate_input_list.py
+++ conversation_reconstruction_dataflow/ingest_revisions/truncate_input_list.py
@@ -1,3 +1,27 @@
+"""
+Copyright 2017 Google Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+-------------------------------------------------------------------------------
+
+  Divide the full input list with all dumps into batches.
+
+  Input: input list with all dumps
+  Output: batched input list. Each batched input list contain batchsize(current value: 20) dumps.
+  Statistics: 24 batches of input lists are generated in the format: 7z_file_list_batched_(batch number).txt under the input_lists folder in dataflow storage.
+  Note: This file was running locally to generate the results, the divided input lists were mannually uploaded to cloud storage.
+"""
+
 folder = 'input_lists/'
 full_list = '7z_file_list_exclude_10.txt'
 total = []
