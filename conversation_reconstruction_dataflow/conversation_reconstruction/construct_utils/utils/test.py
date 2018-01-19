01e58f74362f924c4c67e01ec7e97f005be6cea8
Yiqing <h931125@gmail.com>
2018-01-20 00:12:28 +0800

    address comments in PR

--- conversation_reconstruction_dataflow/conversation_reconstruction/construct_utils/utils/test.py
+++ conversation_reconstruction_dataflow/conversation_reconstruction/construct_utils/utils/test.py
@@ -1,8 +1,25 @@
-from deltas.tokenizers import text_split
-from rev_clean import clean
+"""
+Copyright 2017 Google Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+-------------------------------------------------------------------------------
+"""
+
+from .third_party.deltas.tokenizers import text_split
+from .third_party.rev_clean import clean
 from diff import diff_tuning
 
-from deltas.algorithms import sequence_matcher
+from .third_party.deltas.algorithms import sequence_matcher
 
 def convert_diff_format(x, a, b):
     ret = {}
