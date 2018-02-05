3b566cd80f8a5aa450c01ca920a141a14ce65a99
Yiqing <h931125@gmail.com>
2018-02-04 20:45:06 -0500

    page state append

--- conversation_reconstruction_dataflow/conversation_reconstruction/dataflow_main.py
+++ conversation_reconstruction_dataflow/conversation_reconstruction/dataflow_main.py
@@ -45,7 +45,7 @@ def run(known_args, pipeline_args):
   """Main entry point; defines and runs the reconstruction pipeline."""
 
   pipeline_args.extend([
-    '--runner=DirectRunner',
+    '--runner=DataflowRunner',
     '--project=wikidetox-viz',
     '--staging_location=gs://wikidetox-viz-dataflow/staging',
     '--temp_location=gs://wikidetox-viz-dataflow/tmp',
@@ -60,16 +60,17 @@ def run(known_args, pipeline_args):
   renaming = "latest.rev_id_in_int as rev_id_in_int, latest.week as week, latest.year as year, latest.sha1 as sha1, latest.user_id as user_id, latest.format as format, latest.user_text as user_text, latest.timestamp as timestamp, latest.text as text, latest.page_title as page_title, latest.model as model, latest.page_namespace as page_namespace, latest.page_id as page_id, latest.rev_id as rev_id, latest.comment as comment, latest.user_ip as user_ip, latest.truncated as truncated, latest.records_count as records_count, latest.record_index as record_index"
   get_max_query= "SELECT * FROM (SELECT * FROM %s, (SELECT %s FROM (SELECT %s FROM %s as latest INNER JOIN (SELECT max(rev_id_in_int) as max_rev_id FROM %s WHERE week < %d or year < %d GROUP BY week, year, page_id) as previous_max"%(known_args.input_table, renaming, renaming, known_args.input_table, known_args.input_table, known_args.week, known_args.year)
   get_latest_query = " ON previous_max.max_rev_id == latest.rev_id_in_int) as latest LEFT JOIN (SELECT page_id FROM %s WHERE week == %d and year == %d) as cur ON latest.page_id == cur.page_id)) as ingested"%(known_args.input_table, known_args.week, known_args.year) 
-  join_query = " LEFT JOIN (SELECT ps_tmp.* FROM %s as ps_tmp INNER JOIN (SELECT MAX(rev_id) as max_rid, page_id FROM %s GROUP BY page_id) as tmp ON tmp.page_id == ps_tmp.page_id and ps_tmp.rev_id == tmp.max_rid) as page_state ON ingested.page_id == page_state.ps_tmp.page_id WHERE week==%d and year==%d ORDER BY rev_id_in_int"%(known_args.input_page_state_table,known_args.input_page_state_table,known_args.week, known_args.year)
-  read_query = get_max_query + get_latest_query + join_query 
+  join_query = " LEFT JOIN (SELECT ps_tmp.* FROM %s as ps_tmp INNER JOIN (SELECT MAX(rev_id) as max_rid, page_id FROM %s GROUP BY page_id) as tmp ON tmp.page_id == ps_tmp.page_id and ps_tmp.rev_id == tmp.max_rid) as page_state ON ingested.page_id == page_state.ps_tmp.page_id"%(known_args.input_page_state_table,known_args.input_page_state_table)
+  condition_query = " WHERE week==%d and year==%d ORDER BY rev_id_in_int"%(known_args.week, known_args.year)
+  read_query = get_max_query + get_latest_query + join_query + condition_query
   with beam.Pipeline(options=pipeline_options) as p:
 
     # Read the text file[pattern] into a PCollection.
-    reconstruction_results, page_states = (p | beam.io.Read(beam.io.BigQuerySource(query=get_max_query+get_latest_query+join_query, validate=True)) 
-                   | beam.Map(lambda x: (x['page_id'], x))
+    reconstruction_results, page_states = (p | beam.io.Read(beam.io.BigQuerySource(query=read_query, validate=True)) 
+                   | beam.Map(lambda x: (x['ingested_page_id'], x))
                    | beam.GroupByKey()
                    | beam.ParDo(ReconstructConversation()).with_outputs('page_states', main = 'reconstruction_results'))
-    page_states | "WritePageStates" >> beam.io.Write(bigquery_io.BigQuerySink(known_args.page_states_output_table, schema=known_args.page_states_output_schema, validate=True))
+    page_states | "WritePageStates" >> beam.io.Write(bigquery_io.BigQuerySink(known_args.page_states_output_table, schema=known_args.page_states_output_schema, write_disposition='WRITE_APPEND', validate=True))
     reconstruction_results | "WriteReconstructedResults" >> beam.io.Write(bigquery_io.BigQuerySink(known_args.output_table, schema=known_args.output_schema, validate=True))
 
 
@@ -95,7 +96,7 @@ class ReconstructConversation(beam.DoFn):
     last_page_state = None
     first_time = True
     for cur_revision in rows:
-        cur_revision = self.QueryResult2json(row)
+        cur_revision = self.QueryResult2json(cur_revision)
           
         if cur_revision['record_index'] == 0: 
            revision = cur_revision
@@ -104,7 +105,7 @@ class ReconstructConversation(beam.DoFn):
         if cur_revision['record_index'] == cur_revision['records_count'] - 1:
            if first_time:
               first_time = False
-              if row['page_state']:
+              if cur_revision['page_state']:
                  processor.load(cur_revision['page_state'], cur_revision['deleted_comments'], cur_revision['conversation_id'], cur_revision['authors'], cur_revision['text'])
               continue
            cnt += 1
@@ -146,7 +147,7 @@ if __name__ == '__main__':
   page_states_output_schema = 'rev_id:INTEGER, page_id:STRING, page_state:STRING, deleted_comments:STRING, conversation_id:STRING, authors:STRING'  
   parser.add_argument('--page_states_output_table',
                       dest='page_states_output_table',
-                      default='wikidetox-viz:wikidetox_conversations.new_page_states',
+                      default='wikidetox-viz:wikidetox_conversations.page_states',
                       help='Output page state table for reconstruction.')
   parser.add_argument('--page_states_output_schema',
                       dest='page_states_output_schema',
