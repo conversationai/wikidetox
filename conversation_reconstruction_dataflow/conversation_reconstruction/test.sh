ae8183165e736c83731c2a4d57e65d433ee4d54f
Yiqing <h931125@gmail.com>
2018-02-15 22:35:04 -0500

    update documentation

--- conversation_reconstruction_dataflow/conversation_reconstruction/test.sh
+++
@@ -1,1 +0,0 @@
-python dataflow_main.py --input_table wikidetox_conversations.ingested_test --input_page_state_table wikidetox_conversations.page_states_test --last_revision_table wikidetox_conversations.test_last_revision --page_states_output_table  wikidetox_conversations.page_states_test --category test --week 1 --year 2003 --load_input_from_cloud --setup_file ./setup.py
