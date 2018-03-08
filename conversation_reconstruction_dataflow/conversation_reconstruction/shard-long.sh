2d83085ac74ce193791e59a79208b4a7db057822
Yiqing <h931125@gmail.com>
2018-03-08 12:11:52 -0500

    start sharding

---
+++ conversation_reconstruction_dataflow/conversation_reconstruction/shard-long.sh
@@ -0,0 +1,3 @@
+python shard.py --category longpages --input gs://wikidetox-viz-dataflow/ingested_long_pages/ingested-*.avro --output gs://wikidetox-viz-dataflow/sharded_ingested_long_pages/ --setup_file ./setup.py
+
+
