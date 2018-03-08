ffc508afe0b02f7c81588d2d110a23a8628d822a
Yiqing <h931125@gmail.com>
2018-03-08 15:58:41 -0500

    ingested sharded

---
+++ conversation_reconstruction_dataflow/conversation_reconstruction/shard-short.sh
@@ -0,0 +1,3 @@
+python shard.py --category shortpages --input gs://wikidetox-viz-dataflow/ingested_short_pages/ingested-*.avro --output gs://wikidetox-viz-dataflow/sharded_ingested_short_pages/ingested-000000000004.avro --setup_file ./setup.py
+
+
