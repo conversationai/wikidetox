10e59e46637185d872ee56b29138f364c1b854d5
Yiqing <h931125@gmail.com>
2018-04-25 19:31:15 -0400

    finished pipeline

--- conversation_reconstruction_dataflow/conversation_reconstruction/helper_shell/reconstruct-long.sh
+++ conversation_reconstruction_dataflow/conversation_reconstruction/helper_shell/reconstruct-long.sh
@@ -1,10 +1,11 @@
-for i in $(seq 47 53)
+cd ..
+for i in $(seq 1 53)
 do
 #    gsutil -m rm gs://wikidetox-viz-dataflow/process_tmp/longpages_bakup/*
     gsutil -m mv -p gs://wikidetox-viz-dataflow/process_tmp/longpages/* gs://wikidetox-viz-dataflow/process_tmp/longpages_bakup/
     gsutil -m mv -p gs://wikidetox-viz-dataflow/process_tmp/next_longpages/* gs://wikidetox-viz-dataflow/process_tmp/longpages/
     echo "start job on week $i"
-    python dataflow_main.py --category longpages --input gs://wikidetox-viz-dataflow/sharded_ingested_long_pages/date-{week}at{year}/revisions-*.avro --week $i --year 2009 --setup_file ./setup.py 
+    python dataflow_main.py --category longpages --input gs://wikidetox-viz-dataflow/sharded_ingested_long_pages/date-{week}at{year}/revisions-*.avro --week $i --year 2017 --setup_file ./setup.py 
 done
 
 
