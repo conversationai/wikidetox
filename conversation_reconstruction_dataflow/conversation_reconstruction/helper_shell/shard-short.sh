dddd9408aab950e5d3d050a7718218eb8a800a80
Yiqing Hua <yiqingh@yiqing.c.googlers.com>
2018-05-23 19:34:29 +0000

    updated README

--- conversation_reconstruction_dataflow/conversation_reconstruction/helper_shell/shard-short.sh
+++
@@ -1,5 +0,0 @@
-cd ..
-#python shard.py --category shorpages --input gs://wikidetox-viz-dataflow/sharded_ingested_short_pages/date-2017/revisions-1.avro --output gs://wikidetox-viz-dataflow/sharded_ingested_short_pages/ --setup_file ./setup.py
-python shard.py --category shortpages --input gs://wikidetox-viz-dataflow/ingested_short_pages/ingested-*.avro --output gs://wikidetox-viz-dataflow/sharded_ingested_short_pages/ --setup_file ./setup.py
-
-
