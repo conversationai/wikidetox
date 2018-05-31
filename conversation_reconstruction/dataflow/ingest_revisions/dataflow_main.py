a600045442fd1042a1a472c0a83024eaf16dce42
Yiqing Hua <yiqingh@yiqing.c.googlers.com>
2018-05-31 20:25:53 +0000

    add testmode

--- conversation_reconstruction/dataflow/ingest_revisions/dataflow_main.py
+++ conversation_reconstruction/dataflow/ingest_revisions/dataflow_main.py
@@ -45,6 +45,7 @@ import os
 import time
 import urllib2
 import urllib
+import subprocess
 
 from HTMLParser import HTMLParser
 import re
@@ -63,8 +64,14 @@ from datetime import datetime
 def run(known_args, pipeline_args, sections):
   """Main entry point; defines and runs the ingestion pipeline."""
 
+  if known_args.testmode:
+    # In testmode, disable cloud storage backup and run on directRunner
+    pipeline_args.append('--runner=DirectRunner')
+    known_args.raw = None
+  else:
+    pipeline_args.append('--runner=DataflowRunner')
+
   pipeline_args.extend([
-    '--runner=DataflowRunner',
     '--project=wikidetox-viz',
     '--staging_location=gs://wikidetox-viz-dataflow/staging',
     '--temp_location=gs://wikidetox-viz-dataflow/tmp',
@@ -75,38 +82,47 @@ def run(known_args, pipeline_args, sections):
   pipeline_options = PipelineOptions(pipeline_args)
   pipeline_options.view_as(SetupOptions).save_main_session = True
   with beam.Pipeline(options=pipeline_options) as p:
-    pcoll = (p | "GetDataDumpList" >> beam.Create(sections)
-            | "Ingestion" >> beam.ParDo(WriteDecompressedFile(), known_args.dumpdate, known_args.language)
+    pcoll = (p | "GetDataDumpList" >> beam.Create(sections))
+    if known_args.fromWiki:
+       pcoll = (pcoll | "DownloadDataDumps" >> beam.ParDo(DownloadDataDumps(), known_args.raw))
+    pcoll = ( pcoll | "Ingestion" >> beam.ParDo(WriteDecompressedFile(), known_args.raw)
+            | "Window" >> beam.WindowInto(beam.window.FixedWindows(10, 0))
             | "AddGroupByKey" >> beam.Map(lambda x: ('{week}at{year}'.format(week=x['week'], year=x['year']), x))
-            | "Window" >> beam.WindowInto(beam.window.FixedWindows(10))
             | "ShardByWeek" >>beam.GroupByKey()
             | "WriteToStorage" >> beam.ParDo(WriteToStorage(), known_args.output, known_args.dumpdate, known_args.language))
+
+class DownloadDataDumps(beam.DoFn):
+  def process(self, element, output = None):
+    """Downloads a data dump file, store in cloud storage.
+       Returns the cloud storage location.
+    """
+    mirror, chunk_name =  element
+    logging.info('USERLOG: Download data dump %s to store in cloud storage.' % chunk_name)
+    # Download data dump from Wikipedia and upload to cloud storage.
+    url = mirror + "/" + chunk_name
+    urllib.urlretrieve(url, chunk_name)
+    if output:
+       # output maybe None in test mode
+       dump_path = output + chunk_name
+       os.system("gsutil cp %s %s"%(chunk_name, dump_path))
+    yield chunk_name
 
 class WriteDecompressedFile(beam.DoFn):
   def __init__(self):
       self.processed_revisions = Metrics.counter(self.__class__, 'processed_revisions')
 
-  def process(self, element, dumpdate = None, language = None):
-    """Downloads a data dump file, store in cloud storage.
-       Ingests the xml dump into json, returns the josn records
+  def process(self, element, input = None):
+    """Ingests the xml dump into json, returns the josn records
     """
-    mirror, chunk_name =  element
-
-    logging.info('USERLOG: Download data dump %s to cloud storage.' % chunk_name)
-    # Download and upload the raw data dump
-    dump_path = path.join('gs://wikidetox-viz-dataflow/raw-downloads/%s-%s/'%(language, dumpdate), chunk_name)
-    url = mirror + "/" + chunk_name
-    if not(os.path.exists(chunk_name)): # For local testing efficiency
-       if filesystems.FileSystems.exists(dump_path):
-          os.system("gsutil cp %s %s"%(dump_path, chunk_name))
-       else:
-          urllib.urlretrieve(url, chunk_name)
-          os.system("gsutil cp %s %s"%(chunk_name, dump_path))
     # Decompress the data dump
+    chunk_name = element
+    if input:
+       # Input maybe None in testmode
+       os.system("gsutil cp %s %s"%(input + chunk_name, chunk_name))
+
     logging.info('USERLOG: Running ingestion process on %s' % chunk_name)
     input_file = chunk_name[:-3]
-    if not(os.path.exists(input_file)): # For local testing
-       os.system("7z x %s"%chunk_name)
+    os.system("7z x %s"%chunk_name)
     # Running ingestion on the xml file
     last_revision = 'None'
     last_completed = time.time()
@@ -167,30 +183,64 @@ def directory(mirror):
 
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
+  # Define parameters
   parser = argparse.ArgumentParser()
+  parser.add_argument('--testmode',
+                      dest='testmode',
+                      action="store_true",
+                      help='Turn on the test mode.')
   parser.add_argument('--output',
                       dest='output',
                       default='gs://wikidetox-viz-dataflow/ingested',
                       help='Specify the output storage in cloud.')
+  parser.add_argument('--ingestFromWikipedia',
+                      dest='fromWiki',
+                      action='store_true',
+                      help='If this is set to true, please provide the language and dumpdate of the data dump, otherwise please provide the data storage location.')
+  parser.add_argument('--rawdownload',
+                      dest='raw',
+                      default='gs://wikidetox-viz-dataflow/raw-downloads/en-20180501/',
+                      help='Specify the data storage location to store/stores the raw downloads.')
   parser.add_argument('--language',
                       dest='language',
                       default='en',
                       help='Specify the language of the Wiki Talk Page you want to ingest.')
   parser.add_argument('--dumpdate',
                       dest='dumpdate',
-                      default='latest',
+                      default='20180501',
                       help='Specify the date of the Wikipedia data dump.')
   known_args, pipeline_args = parser.parse_known_args()
-  dumpstatus_url = 'https://dumps.wikimedia.org/{lan}wiki/{date}/dumpstatus.json'.format(lan=known_args.language, date=known_args.dumpdate)
-  try:
-     response = urllib2.urlopen(dumpstatus_url)
-     dumpstatus = json.loads(response.read())
-  except:
-     dumpstatus = None
-  if not(dumpstatus):
-     mirror = 'http://dumps.wikimedia.your.org/{lan}wiki/latest'.format(lan=known_args.language)
-     sections = directory(mirror)
+
+  if known_args.testmode:
+     # The test mode is running on a relatively small data dump if downloading
+     # from Wikipedia directly..
+     known_args.language = 'ch'
+     known_args.dumpdate = 'latest'
+
+  if known_args.fromWiki:
+     # If specified downloading from Wikipedia
+     dumpstatus_url = 'https://dumps.wikimedia.org/{lan}wiki/{date}/dumpstatus.json'.format(lan=known_args.language, date=known_args.dumpdate)
+     try:
+        response = urllib2.urlopen(dumpstatus_url)
+        dumpstatus = json.loads(response.read())
+        url = 'https://dumps.wikimedia.org/{lan}wiki/{date}'.format(lan=known_args.language, date=known_args.dumpdate)
+        sections = [(url, filename) for filename in dumpstatus['jobs']['metahistory7zdump']['files'].keys()]
+     except:
+        # In the case dumpdate is not specified or is invalid, download the
+        # latest version.
+        mirror = 'http://dumps.wikimedia.your.org/{lan}wiki/latest'.format(lan=known_args.language)
+        sections = directory(mirror)
   else:
-     url = 'https://dumps.wikimedia.org/{lan}wiki/{date}'.format(lan=known_args.language, date=known_args.dumpdate)
-     sections = [(url, filename) for filename in dumpstatus['jobs']['metahistory7zdump']['files'].keys()]
+     if known_args.testmode:
+        # In testmode when not downloading from Wikipedia directly, test on the
+        # testdata
+        sections = ['ingest_utils/testdata/test_wiki_dump.xml.7z']
+     else:
+        # Otherwise, fetch the list of files in the cloud storage
+        os.system("gsutil ls %s > tmp"%known_args.raw)
+        sections = []
+        with open("tmp", "r") as f:
+            for line in f:
+              sections.append(line[len(known_args.raw):-1])
   run(known_args, pipeline_args, sections)
+
