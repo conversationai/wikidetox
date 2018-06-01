8aa622d76b84944ca92ff9d0904ddb239bab8a54
Yiqing Hua <yiqingh@yiqing.c.googlers.com>
2018-06-01 21:58:11 +0000

    streaming

--- conversation_reconstruction/dataflow/ingest_revisions/dataflow_tester.py
+++ conversation_reconstruction/dataflow/ingest_revisions/dataflow_tester.py
@@ -47,50 +47,57 @@ from apache_beam.testing.util import equal_to
 from apache_beam.testing.test_utils import compute_hash
 
 class TestParDo(unittest.TestCase):
+    def init(self):
+        pipeline_args = [
+            '--runner=DirectRunner',
+            '--project=wikidetox-viz',
+            '--staging_location=gs://wikidetox-viz-dataflow/staging',
+            '--temp_location=gs://wikidetox-viz-dataflow/tmp',
+            '--job_name=test-ingestion-pipeline', 
+            '--num_workers=30',
+        ]
+        test_url  = [('http://dumps.wikimedia.your.org/chwiki/latest/', 'chwiki-latest-pages-meta-history.xml.bz2')]
+        return pipeline_args, test_url
+
     def create_temp_file(self):
       with tempfile.NamedTemporaryFile(delete=False) as f:
         return f.name
 
-    def test_pardo(self):
-      pipeline_args = [
-        '--runner=DirectRunner',
-        '--project=wikidetox-viz',
-        '--staging_location=gs://wikidetox-viz-dataflow/staging',
-        '--temp_location=gs://wikidetox-viz-dataflow/tmp',
-        '--job_name=test-ingestion-pipeline', 
-        '--num_workers=30',
-      ]
-      test_url  = [('http://dumps.wikimedia.your.org/chwiki/latest/', 'chwiki-latest-pages-meta-history.xml.7z')]
-      ans_hash = "8f32f3812f123411dafe9e715e34477df26d434e"
+    def test_download(self):
+      pipeline_args, test_url = self.init()
       temp_path = self.create_temp_file()
-
       pipeline_options = PipelineOptions(pipeline_args)
       pipeline_options.view_as(SetupOptions).save_main_session = True
 
       # Test Download
       with TestPipeline(options=pipeline_options) as p:
         p = (p | beam.Create(test_url)
-               | beam.ParDo(DownloadDataDumps())
+             | beam.ParDo(DownloadDataDumps(), 'wikidetox-viz-dataflow/test_ingestion/')
                | beam.io.WriteToText("%s"%temp_path, num_shards=1))
       results = []
       with open("%s-00000-of-00001"%temp_path) as result_file:
         for line in result_file:
           results.append(line[:-1])
-      self.assertEqual(''.join(results), 'chwiki-latest-pages-meta-history.xml.7z')
+      self.assertEqual(''.join(results), 'chwiki-latest-pages-meta-history.xml')
 
+    def test_ingest(self):
+      pipeline_args, test_url = self.init()
+      temp_path = self.create_temp_file()
+      pipeline_options = PipelineOptions(pipeline_args)
+      pipeline_options.view_as(SetupOptions).save_main_session = True
       # Test Ingestion
       with TestPipeline(options=pipeline_options) as p:
-        p = (p | beam.Create(['ingest_utils/testdata/test_wiki_dump.xml.7z'])
-               | beam.ParDo(WriteDecompressedFile())
+        p = (p | beam.Create(['ingest_utils/testdata/test_wiki_dump.xml'])
+               | beam.ParDo(WriteDecompressedFile(), None, 'local')
                | beam.io.WriteToText("%s"%temp_path, num_shards=1))
       results = []
       with open("%s-00000-of-00001"%temp_path) as result_file:
         for line in result_file:
           results.append(line[:-1])
       self.assertEqual(len(results), 5)
-
-      # Test Full Pipeline
-      os.system('python dataflow_main.py --testmode --output gs://wikidetox-viz-dataflow/test/')
+   # def test_full(self):
+   #   # Test Full Pipeline
+   #   os.system('python dataflow_main.py --testmode --output gs://wikidetox-viz-dataflow/test/')
 
 if __name__ == '__main__':
    logging.getLogger().setLevel(logging.INFO)
