e6806334ce22625a8a661fb612b13f9464dd00fe
Yiqing Hua <yiqingh@yiqing.c.googlers.com>
2018-06-28 14:48:44 +0000

    using cloud storage for revisions

--- conversation_reconstruction/dataflow/conversation_reconstruction/helper_shell/shard-long.sh
+++
@@ -1,4 +0,0 @@
-cd ..
-python shard.py --category longpages --input gs://wikidetox-viz-dataflow/ingested_long_pages/ingested-*.avro --output gs://wikidetox-viz-dataflow/sharded_ingested_long_pages/ --setup_file ./setup.py
-
-
--- conversation_reconstruction/dataflow/conversation_reconstruction/helper_shell/test.sh
+++ conversation_reconstruction/dataflow/conversation_reconstruction/helper_shell/test.sh
@@ -1,5 +1,6 @@
 cd ..
-gsutil -m mv -p gs://wikidetox-viz-dataflow/process_tmp/current/* gs://wikidetox-viz-dataflow/process_tmp/bakup/
-gsutil -m mv -p gs://wikidetox-viz-dataflow/process_tmp/next_stage/* gs://wikidetox-viz-dataflow/process_tmp/current/
-
-python dataflow_main.py --input gs://wikidetox-viz-dataflow/ingested/latest-ch/date-11at2007/revisions*.json --week 11 --year 2007 --setup_file ./setup.py --testmode
+gsutil rm gs://wikidetox-viz-dataflow/test_tmp/current/*
+gsutil -m cp helper_shell/empty_file gs://wikidetox-viz-dataflow/test_tmp/current/last_rev
+gsutil -m cp helper_shell/empty_file gs://wikidetox-viz-dataflow/test_tmp/current/page_states
+gsutil -m cp helper_shell/empty_file gs://wikidetox-viz-dataflow/test_tmp/current/error_log
+python dataflow_main.py --input gs://wikidetox-viz-dataflow/ingested/en-20180501/20180501-en/date-5at2001/revisions*.json --week 5 --year 2001 --setup_file ./setup.py --process_file test_tmp --output_name test --testmode
