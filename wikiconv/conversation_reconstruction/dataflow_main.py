615a36f978948a2262c618965ea7dcd36813d012
Jeffrey Sorensen <sorenj@google.com>
2019-04-20 10:52:05 -0400

    Multiple changes reflecting the complexity of doing recursive filecopy.

--- wikiconv/conversation_reconstruction/dataflow_main.py
+++ wikiconv/conversation_reconstruction/dataflow_main.py
@@ -304,7 +304,7 @@ class Locations:
       known_args.output_state + "/dataflow_tmp/temp/")
 
     self.output_revs_with_marks = (
-      known_args.output_state + "/revs_with_marks/revs_with_marks")
+      known_args.output_state + "/revs_with_marks")
     self.output_page_states = (
       known_args.output_state + "/page_states/page_states")
     self.output_last_revisions = (
