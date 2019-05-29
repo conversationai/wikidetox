3116db52c83b170dbe71ae0d384908b602057a8e
Tamajong Nchukwi <tamajongnc@google.com>
2019-05-29 15:28:58 -0400

    reading api key from file

--- antidox/perspective.py
+++ antidox/perspective.py
@@ -1,14 +1,19 @@
 from googleapiclient import discovery
+import json
 
+with open("api_key.json") as json_file:
+	apikey_data = json.load(json_file)
+
+API_KEY = apikey_data['perspective_key']
 # Generates API client object dynamically based on service name and version.
-service = discovery.build('commentanalyzer', 'v1alpha1', developerKey=API_KEY)
+perspective = discovery.build('commentanalyzer', 'v1alpha1', developerKey=API_KEY)
 
 analyze_request = {
   'comment': { 'text': 'friendly greetings from python' },
   'requestedAttributes': {'TOXICITY': {}}
 }
 
-response = service.comments().analyze(body=analyze_request).execute()
+response = perspective.comments().analyze(body=analyze_request).execute()
 
 import json
 print (json.dumps(response, indent=2))
