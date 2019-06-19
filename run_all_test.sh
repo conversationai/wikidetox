226b25f3a0c44ef7f32ae290914fd10c2e4ab07e
Jeffrey Sorensen <sorenj@google.com>
2019-06-19 14:04:25 -0400

    Run correct test.

--- run_all_test.sh
+++ run_all_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+set -x
 
 python -m antidox.wikiwatcher_test
 python -m antidox.perspective_test
-python -m wikiconv.ingest_revisions.dataflow_tester
+python -m wikiconv.ingest_revisions.ingester_test
 python -m wikiconv.conversation_reconstruction.construct_utils.conversation_constructor_test
 python -m wikiconv.conversation_reconstruction.construct_utils.reconstruct_conversation_test
 python -m wikiconv.conversation_reconstruction.dataflow_test
