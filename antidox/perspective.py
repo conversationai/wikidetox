651e2bcff96949c5ddff7bead34c5b0dfc3388ea
Tamajong Nchukwi <tamajongnc@google.com>
2019-06-24 17:49:46 -0400

    using apache beam for batch processing

--- antidox/perspective.py
+++ antidox/perspective.py
@@ -1,14 +1,18 @@
 """ inputs comments to perspective and dlp apis and detects
 toxicity and personal information> has support for csv files,
 bigquery tables, and wikipedia talk pages"""
+#TODO(tamajongnc): configure pipeline to distribute work to multiple machines
+#TODO(tamajongnc): use windowing technique to accomodate large and continuous data sets
+# pylint: disable=fixme, import-error
+# pylint: disable=fixme, unused-import
 import argparse
 import json
 import sys
-import pandas as pd
 from googleapiclient import discovery
 from googleapiclient import errors as google_api_errors
-from google.cloud import bigquery
-
+import apache_beam as beam
+from apache_beam.io.gcp.internal.clients import bigquery
+from apache_beam.options.pipeline_options import PipelineOptions
 
 def get_client(api_key_filename):
   """ generates API client with personalized API key """
@@ -114,7 +118,7 @@ def use_query(content, sql_query, big_q):
   rows = query_job.result()
   strlst = []
   for row in rows:
-    strlst.append(str(row[content]))
+    strlst.append(row[content])
   return strlst
 
 # pylint: disable=fixme, too-many-locals
@@ -126,39 +130,51 @@ def main(argv):
   parser.add_argument('--sql_query', help='choose specifications for query search')
   parser.add_argument('--csv_file', help='input csv file')
   parser.add_argument('--content', help='specify a column in dataset to retreive data from')
+  parser.add_argument('--output', help='path for output file')
+  parser.add_argument('--suffix', help='output file suffix')
+  parser.add_argument('--project', help='project id for bigquery table')
   args = parser.parse_args(argv)
   apikey_data, perspective, dlp = get_client('api_key.json')
-  if args.csv_file:
-    text = pd.read_csv(args.csv_file)
-  if args.sql_query:
-    big_q = bigquery.Client.from_service_account_json('querykey.json')
-    text = use_query(args.content, args.sql_query, big_q)
-  pii_results = open("pii_results.txt", "w+")
-  toxicity_results = open("toxicity_results.txt", "w+")
-  print(big_q)
-
-  for line in text:
-    try:
-      print(line)
-      print('==============================================\n')
-      dlp_response = dlp_request(dlp, apikey_data, line)
-      perspective_response = perspective_request(perspective, line)
-      has_pii_bool, pii_type = contains_pii(dlp_response)
-      if has_pii_bool:
-        pii_results.write(str(line)+"\n"+'contains pii?'+"Yes"+"\n"
-                          +str(pii_type)+"\n"
-                          +"==============================================="+"\n")
-      if contains_toxicity(perspective_response):
-        toxicity_results.write(str(line)+"\n" +"contains TOXICITY?:"+
-                               "Yes"+"\n"+
-                               str(perspective_response['attributeScores']
-                                   ['TOXICITY']['summaryScore']['value'])+"\n"
-                               +"=========================================="+"\n")
-    except google_api_errors.HttpError as err:
-      print('error', err)
-  toxicity_results.close()
-  pii_results.close()
-    # print('dlp result:', json.dumps(dlp_response, indent=2))
-    # print ("contains_toxicity:", json.dumps(perspective_response, indent=2))
+  with beam.Pipeline(options=PipelineOptions()) as pipeline:
+    if args.csv_file:
+      comments = pipeline | 'ReadMyFile' >> beam.io.ReadFromText(args.csv_file)
+    # if args.sql_query:
+    comments = (
+        pipeline
+        | 'QueryTable' >> beam.io.Read(beam.io.BigQuerySource(
+            query=args.sql_query,
+            use_standard_sql=True))
+        | beam.Map(lambda elem: elem[args.content]))
+    # pylint: disable=fixme, too-few-public-methods
+    class GetToxicity(beam.DoFn):
+      """The DoFn to perform on each element in the input PCollection"""
+      # pylint: disable=fixme, no-self-use
+      # pylint: disable=fixme, inconsistent-return-statements
+      def process(self, element):
+        """Runs every element of collection through perspective and dlp"""
+        print(element)
+        print('==============================================\n')
+        dlp_response = dlp_request(dlp, apikey_data, element)
+        has_pii_bool, pii_type = contains_pii(dlp_response)
+        try:
+          perspective_response = perspective_request(perspective, element)
+          if has_pii_bool:
+            pii = [element+"\n"+'contains pii?'+"Yes"+"\n"+str(pii_type)+"\n" \
+            +"==============================================="+"\n"]
+            return pii
+          if contains_toxicity(perspective_response):
+            tox = [element+"\n" +"contains TOXICITY?:"+"Yes"
+                   +"\n"+str(perspective_response['attributeScores']
+                             ['TOXICITY']['summaryScore']['value'])+"\n"
+                   +"=========================================="+"\n"]
+            return tox
+        except google_api_errors.HttpError as err:
+          print('error', err)
+    results = comments\
+     | beam.ParDo(GetToxicity())
+    # pylint: disable=fixme, expression-not-assigned
+    results | 'WriteToText' >> beam.io.WriteToText(
+        args.output, \
+         file_name_suffix=args.suffix)
 if __name__ == '__main__':
   main(sys.argv[1:])
